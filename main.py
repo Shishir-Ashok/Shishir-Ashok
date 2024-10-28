985529f10c147a774363cc9480e2d598039cf58a
Shishir Ashok <43123813+Shishir-Ashok@users.noreply.github.com>
2024-10-28 15:13:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ With strong skills in data automation and analysis, I aim to leverage my technic
 ---
 
 ### 📊 GitHub Stats
-- Recent Contribution: [{current_repo}]({url_current_repo})
+- Currently working on: [{current_repo}]({url_current_repo})
 - Repos : {repo_count}
 - Lines of Code: {total_contributions} ($$\\color{{\#2dba4e}}{lines_added}++ \space \space \space \\color{{\#f0440a}}{lines_removed}--$$)
 
