1e1e5e04a8615b7a0bc27467e0323b1bee3b9d76
Shishir Ashok <43123813+Shishir-Ashok@users.noreply.github.com>
2024-11-03 18:30:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import requests
+import random
 import os
 
-# GitHub API headers
-HEADERS = {'authorization': 'token ' + os.environ['ACCESS_TOKEN']}
-user = os.environ['user']
 
+ip_list = [
+"113.127.68.11",
+"10.211.119.132",
+"54.84.122.206",
+"203.42.252.207",
+"70.73.191.224",
+"37.3.163.239",
+"255.156.156.189",
+"233.227.58.48",
+"181.71.235.83",
+"150.20.12.106"
+]
+
+
+# GitHub API headers
+HEADERS = {'authorization': 'token ' + os.environ['ACCESS_TOKEN'], 'X-Forwarded-For': random.choice(ip_list)}
+user = os.environ['user']
 
 def request_call(query):
     variables =  {'login': user}
